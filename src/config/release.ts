760fe2e61849f1817a15519f6aac09ea76af3eff
StasHolmansky <holmansky5@gmail.com>
2026-07-28 09:56:53 +0300

    Bump version to 1.0.2 (3) for closed testing release

--- src/config/release.ts
+++ src/config/release.ts
@@ -1,4 +1,4 @@
 export const APP_DISPLAY_NAME = 'Currency Converter';
 export const SUPPORT_EMAIL = 'stanislavkholmanskii@gmail.com';
-export const APP_VERSION = '1.0.1';
-export const APP_BUILD = '2';
+export const APP_VERSION = '1.0.2';
+export const APP_BUILD = '3';
